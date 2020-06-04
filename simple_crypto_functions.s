c83676fdce27f5ae807860068bd4d36d70dcf44c
PapiSenpai <juan03192001@gmail.com>
2020-06-03 23:07:10 -0700

    Assignment

--- simple_crypto_functions.s
+++ simple_crypto_functions.s
@@ -12,7 +12,7 @@
 ///
 .align 4
 encrypt:
-	push {r4, r5, r6, r7, LP} // step #1_en - Save R4,R5,R6,R7, and LR
+	push {r4, r5, r6, r7, LR} // step #1_en - Save R4,R5,R6,R7, and LR
 	mov r4, #0 // step #2_en - Set register R4 to 0
 	mov r5, #0 // step #3_en - Set register R5 to 0
 
@@ -21,11 +21,11 @@ EN_WHL_R5_LT_R1:
 	blGE r5, EN_END_WHL_R5_LT_R1 // step #5_en - if R5 greater than or equal to R1, branch to EN_END_WHL_R5_LT_R1
 	ldrb r6, r0[r5] // step #6_en - load byte into register R6, the contents of R0[R5]
 	ldrb r7, r2[r4] // step #7_en - load byte into register R7, the contents of R2[R4]
-	OR r6, r6, r7 // step #8_en - exclusive or R6 and R7, storing result in R6
+	XOR r6, r7 // step #8_en - exclusive or R6 and R7, storing result in R6
 	AND r7, r5, #255 // step #9_en - use bitwise AND with R5 and #255 to cast R5's value as char instead of int, store in R7
 	add r6, r6, r7 // step #10_en - add R6 and R7, storing result in R6
 	strb r6, r0[r5] // step #11_en - store byte in R6 to R0[R5]
-	inc r4 // step #12_en - increment R4 by one
+	inc r4, #1 // step #12_en - increment R4 by one
 EN_IF_R4_GE_R3:
 	cmp r4, r3 // step #13_en - compare R4 and R3 in this order of the registers
 	movGE r4, #0// step #14_en - do conditional execution, move if greater than or equal, into R4 the value #0
@@ -56,8 +56,8 @@ DE_WHL_R5_LT_R1:
 	ldrb r6, r0[r5]// step #6_de - load byte into register R6, the contents of R0[R5]
 	AND r7, r5, #255 // step #7_de - use bitwise AND with R5 and #255 to cast R5's value as char instead of int, store in R7
 	sub r6, r6, r7 // step #8_de - subtract R6 and R7, storing result in R6
-	ldbr r7, r2[r4] // step #9_de - load byte into register R7, the contents of R2[R4]
-	OR r6, r6, r7// step #10_de - exclusive or R6 and R7, storing result in R6
+	ldrb r7, r2[r4] // step #9_de - load byte into register R7, the contents of R2[R4]
+	XOR r6, r7 // step #10_de - exclusive or R6 and R7, storing result in R6
 	strb r6, r0[r5] // step #11_de - store byte in R6 to R0[R5]
 	inc r4 // step #12_de - increment R4 by one
 DE_IF_R4_GE_R3:
